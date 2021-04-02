3e81d83d16aebdac11e5102fffe1ace7b18b7226
Francesco Nattino <f.nattino@esciencecenter.nl>
2021-04-02 16:46:13 +0200

    fix issue in script - catalog is now saved at the end

--- image_retrieval/scripts/download-assets/download-assets.py
+++ image_retrieval/scripts/download-assets/download-assets.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 import sys
 
 import pystac
@@ -15,6 +16,7 @@ def _parse_config_file(filename=None):
     Parse input arguments from a config file.
 
     :param filename: path to the config file
+    :return list of parameters
     """
     filename = filename or CONFIG_FILENAME_DEFAULT
 
@@ -23,13 +25,37 @@ def _parse_config_file(filename=None):
     config = parser["download-assets"]
 
     catalog_url = config.pop("catalog_url")
+    collection_id = config.pop("collection_id", None)
     macaroon_path = config.pop("macaroon_path")
     asset_keys = config.pop("asset_keys", "").split()
     max_workers = int(config.pop("max_workers", 1))
 
     assert len(config) == 0, ("Unknown keys: "
                               "{}".format([k for k in config.keys()]))
-    return catalog_url, macaroon_path, asset_keys, max_workers
+    return catalog_url, collection_id, macaroon_path, asset_keys, max_workers
+
+
+def _read_catalog(url):
+    """
+    Read STAC catalog from URL
+
+    :param url: urlpath to the catalog root
+    :return: PyStac Catalog object
+    """
+    url = url if url.endswith("catalog.json") else f"{url}/catalog.json"
+    catalog = pystac.Catalog.from_file(url)
+    return catalog
+
+
+def _save_catalog(catalog, url):
+    """
+    Save STAC catalog to URL
+
+    :param: PyStac Catalog object
+    :param url: urlpath where to save the catalog root
+    """
+    url = url if not url.endswith("catalog.json") else os.path.split(url)[0]
+    catalog.normalize_and_save(url)
 
 
 def download_assets(
@@ -50,6 +76,8 @@ def download_assets(
     """
     asset_keys = asset_keys or [None]
     for asset_key in asset_keys:
+        item = next(catalog.get_all_items())
+        item.assets[asset_key].href = "tmp"
         copy_asset(
             catalog,
             asset_key,
@@ -67,23 +95,25 @@ def main(config_filename):
         input
     """
 
-    # read config file
-    (catalog_url, macaroon_path, asset_keys, max_workers) = \
+    (catalog_url, collection_id, macaroon_path, asset_keys, max_workers) = \
         _parse_config_file(config_filename)
 
     # configure connection to dCache
     dcache_fs = stac2dcache.configure(filesystem="dcache",
                                       token_filename=macaroon_path)
 
-    # read catalog
-    catalog = pystac.Catalog.from_file(catalog_url)
+    catalog = _read_catalog(catalog_url)
+    subcatalog = catalog.get_child(collection_id) \
+        if collection_id is not None else catalog
 
     download_assets(
-        catalog,
+        subcatalog,
         fs_to=dcache_fs,
         asset_keys=asset_keys,
         max_workers=max_workers
     )
+
+    _save_catalog(catalog, catalog_url)
 
 
 if __name__ == "__main__":
